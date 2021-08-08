212512bac2fe53d076b9f886fcc2891eeb6b07b0
WenS1n <865047755@qq.com>
2021-08-08 18:58:02 +0800

    项目初始化

---
+++ gulimall-member/src/main/java/cn/vesns/gulimall/gullimall/member/GullimallMemberApplication.java
@@ -0,0 +1,13 @@
+package cn.vesns.gulimall.gullimall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GullimallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GullimallMemberApplication.class, args);
+    }
+
+}
---
+++ gulimall-member/src/test/java/cn/vesns/gulimall/gullimall/member/GullimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.vesns.gulimall.gullimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GullimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
